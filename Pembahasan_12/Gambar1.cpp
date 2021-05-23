cd8bada0e9c3f5418869062719258a0ffefb64f7
Kitbert <kitbert77@gmail.com>
2021-05-23 17:54:57 +0700

    Create Pembahasan_12 directory.
    - Add Gambar1.cpp and Gambar2.cpp.

---
+++ Pembahasan_12/Gambar1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "----------Gambar 1----------\n\nInorder: 7 9 5 10 6 8 4\nPreorder: 10 9 7 5 8 6 4\nPostorder: 7 5 9 6 4 8 10";
+}
---
+++ Pembahasan_12/Gambar2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "----------Gambar 2----------\n\nInorder: L A H O\nPreorder: H A L O\nPostorder: L A O H";
+}
