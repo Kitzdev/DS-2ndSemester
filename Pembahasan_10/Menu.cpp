8c358ef8af5055367101ecf2be3b2abc38b6c60a
Kitbert <kitbert77@gmail.com>
2021-05-23 14:22:41 +0700

    Create Menu.cpp.

---
+++ Pembahasan_10/Menu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userAnswerI;
+
+    cout << "Menu\n1. Tambah data ke tree\n2. Cari data\n3. Hapus data dari tree - deletion by Merging\n4. Hapus data dari tree - deletion by Copying\n5. Pre-order traversal\n6. Post-order traversal\n7. In-oder traversal\n8. Level-order traversal\n0. Keluar\n\nPilihan Anda : ";
+    cin >> userAnswerI;
+}
